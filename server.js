fb6f2be8a760ca5feb2faf8c693f23447d371f5d
Vshepard879 <vshepard879@gmail.com>
2022-06-26 16:06:49 -0400

    deleted moment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 app.use(require('./routes'));
 
 // connect to mongoose
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-API', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/social-network-api', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true

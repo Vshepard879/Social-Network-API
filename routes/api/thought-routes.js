68abc857989525c6d72cc3ef474f98f03e6f3515
Vshepard879 <vshepard879@gmail.com>
2022-06-26 15:35:33 -0400

    created the Api Routes and index.js route

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../controllers/thought-controller');
+
+// Set up Get all and Post at /api/thoughts
+router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+    
+// Set up Get one, Put, and Delete at /api/thoughts/:id
+router
+    .route('/:id')
+    .get(getThoughtById) 
+    .put(updateThought)
+    .delete(deleteThought);
+    
+// Post at /api/thoughts/:id/reactions
+router
+    .route('/:thoughtId/reactions')
+    .post(createReaction);
+    
+// Delete at /api/thoughts/:id/reactions/:reactionId
+router
+    .route('/:thoughtId/reactions/:reactionId')
+    .delete(deleteReaction);
+
+module.exports = router;    
---
+++ routes/api/user-routes.js
@@ -0,0 +1,30 @@
+const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+// Set up Get all and Post at /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// Set up Get one, Put, and Delete at /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// Add and Delete friends at /api/users/:userId/friends/:friendId
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend);   
